bda2340a9b3d69de4b7885f080a49e87278d3718
Edvin Alestig <edvin.alestig@gmail.com>
2023-12-04 09:54:55 +0100

    Dec 4 2023

---
+++ 2023/4/main.py
@@ -0,0 +1,23 @@
+from functools import reduce
+import re
+
+regex = re.compile("Card +([0-9]+): ([0-9 ]+) \| ([0-9 ]+)")
+
+with open("input.txt") as f:
+    data = f.readlines()
+
+matches = map(lambda x: re.match(regex, x), data)
+numbers = {int(m.group(1)): (set(filter(lambda x: x != "", m.group(2).split(" "))), 
+            set(m.group(3).split(" "))) for m in matches}
+intersections = {card: len(x[0].intersection(x[1])) for card,x in numbers.items()}
+
+# Part 1
+points = map(lambda x: 2**(x-1) if x else 0, intersections.values())
+print(sum(points))
+
+# Part 2
+cards = {card_num: 1 for card_num in intersections.keys()}
+for card,nums in intersections.items():
+    for i in range(nums):
+        cards[card+i+1] += cards[card]
+print(sum(cards.values()))
