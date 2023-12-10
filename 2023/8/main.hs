4e0fe7ba5d358558d87e74d1c7427960d979dfb2
Edvin Alestig <edvin.alestig@gmail.com>
2023-12-10 13:17:27 +0100

    Cleaned up 8th 2023 but still runs out of memory

--- 2023/8/main.hs
+++ 2023/8/main.hs
@@ -1,6 +1,5 @@
 import Data.Text (splitOn, pack, unpack)
-import qualified Data.Map as M
-import Data.Maybe (fromJust)
+import Data.Map ((!), Map, fromList, keys)
 
 main :: IO ()
 main = part2
@@ -9,24 +8,25 @@ part1 :: IO ()
 part1 = do
     [instr, nodes'] <- map unpack . splitOn (pack "\n\n") . pack <$> readFile "input.txt"
     let instructions = cycle instr
-        nodes = M.fromList $ map (\s -> (take 3 s, (take 3 $ drop 7 s, take 3 $ drop 12 s))) $ lines nodes'
+        nodes = fromList $ map (\s -> (take 3 s, (take 3 $ drop 7 s, take 3 $ drop 12 s))) $ lines nodes'
     print $ findZZZ 0 instructions nodes "AAA"
     where
-        findZZZ :: Integer -> String -> M.Map String (String,String) -> String -> Integer
+        findZZZ :: Integer -> String -> Map String (String,String) -> String -> Integer
         findZZZ n _      _     "ZZZ"   = n
         findZZZ n (i:is) nodes current = case i of
-            'L' -> findZZZ (n+1) is nodes $ fst . fromJust $ M.lookup current nodes
-            'R' -> findZZZ (n+1) is nodes $ snd . fromJust $ M.lookup current nodes
+            'L' -> findZZZ (n+1) is nodes $ fst $ nodes ! current
+            'R' -> findZZZ (n+1) is nodes $ snd $ nodes ! current
 
 part2 :: IO () -- Runs out of memory..
 part2 = do
     [instr, nodes'] <- map unpack . splitOn (pack "\n\n") . pack <$> readFile "input.txt"
     let instructions = cycle instr
-        nodes = M.fromList $ map (\s -> (take 3 s, (take 3 $ drop 7 s, take 3 $ drop 12 s))) $ lines nodes'
-        starts = filter (\s -> last s == 'A') $ M.keys nodes
+        nodes = fromList $ map (\s -> (take 3 s, (take 3 $ drop 7 s, take 3 $ drop 12 s))) $ lines nodes'
+        starts = filter (\s -> last s == 'A') $ keys nodes
     print $ findZs 0 instructions nodes starts
 
-findZs :: Integer -> String -> M.Map String (String,String) -> [String] -> Integer
-findZs n (i:is) nodes currents = if not (any (\s -> last s /= 'Z') currents) then n else case i of
-    'L' -> findZs (n+1) is nodes $ map (\curr -> fst . fromJust $ M.lookup curr nodes) currents
-    'R' -> findZs (n+1) is nodes $ map (\curr -> snd . fromJust $ M.lookup curr nodes) currents
+findZs :: Integer -> String -> Map String (String,String) -> [String] -> Integer
+findZs n (i:is) nodes currents = if all (\s -> last s == 'Z') currents then n else
+    case i of
+        'L' -> findZs (n+1) is nodes $ map (\curr -> fst $ nodes ! curr) currents
+        'R' -> findZs (n+1) is nodes $ map (\curr -> snd $ nodes ! curr) currents
