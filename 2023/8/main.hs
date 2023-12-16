0e89f760ef0dfc5b680e92087c54a40fe0706861
Edvin Alestig <edvin.alestig@gmail.com>
2023-12-16 19:38:33 +0100

    Dec 8 2023 part 2

--- 2023/8/main.hs
+++ 2023/8/main.hs
@@ -1,12 +1,9 @@
-import Data.Text (splitOn, pack, unpack)
+import Data.List.Split (splitOn)
 import Data.Map ((!), Map, fromList, keys)
-
-main :: IO ()
-main = part2
 
 part1 :: IO ()
 part1 = do
-    [instr, nodes'] <- map unpack . splitOn (pack "\n\n") . pack <$> readFile "input.txt"
+    [instr, nodes'] <- splitOn "\n\n" <$> readFile "input.txt"
     let instructions = cycle instr
         nodes = fromList $ map (\s -> (take 3 s, (take 3 $ drop 7 s, take 3 $ drop 12 s))) $ lines nodes'
     print $ findZZZ 0 instructions nodes "AAA"
@@ -17,16 +14,24 @@ part1 = do
             'L' -> findZZZ (n+1) is nodes $ fst $ nodes ! current
             'R' -> findZZZ (n+1) is nodes $ snd $ nodes ! current
 
-part2 :: IO () -- Runs out of memory..
+-- Ugly solution and isn't really general. Does not take the instruction length into account
+-- and assumes it only reaches one state ending with Z.
+part2 :: IO ()
 part2 = do
-    [instr, nodes'] <- map unpack . splitOn (pack "\n\n") . pack <$> readFile "input.txt"
+    [instr, nodes'] <- splitOn "\n\n" <$> readFile "input.txt"
     let instructions = cycle instr
         nodes = fromList $ map (\s -> (take 3 s, (take 3 $ drop 7 s, take 3 $ drop 12 s))) $ lines nodes'
         starts = filter (\s -> last s == 'A') $ keys nodes
-    print $ findZs 0 instructions nodes starts
+        loopLengths = map (findZ 0 instructions nodes []) starts
+    print $ foldl1 lcm loopLengths
 
-findZs :: Integer -> String -> Map String (String,String) -> [String] -> Integer
-findZs n (i:is) nodes currents = if all (\s -> last s == 'Z') currents then n else
-    case i of
-        'L' -> findZs (n+1) is nodes $ map (\curr -> fst $ nodes ! curr) currents
-        'R' -> findZs (n+1) is nodes $ map (\curr -> snd $ nodes ! curr) currents
+findZ :: Integer -> String -> Map String (String,String) -> [(Integer,String)] -> String -> Integer
+findZ n (i:is) nodes visited current = do
+    let prevZs = filter (\(n',node) -> current == node) visited
+    if not $ null prevZs then
+        n - fst (head prevZs)
+    else do
+        let visited' = if last current == 'Z' then (n, current):visited else visited
+        case i of
+            'L' -> findZ (n+1) is nodes visited' $ fst $ nodes ! current
+            'R' -> findZ (n+1) is nodes visited' $ snd $ nodes ! current
