001e67d78c36e23bfefebed2cd6e055872f56b34
Edvin Alestig <edvin.alestig@gmail.com>
2024-11-28 23:49:43 +0100

    Dec 1 2022 C# version

---
+++ 2022/day1/csharp/dec1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using System.IO;
+
+String line;
+try
+{
+    StreamReader reader = new StreamReader("../../../input.txt");
+    line = reader.ReadLine();
+
+    int currMax = 0;
+    int cal = 0;
+    List<int> list = new();
+
+    while (line != null)
+    {
+        if (line == "")
+        {
+            currMax = Math.Max(currMax, cal);
+            list.Add(cal);
+            cal = 0;
+        }
+        else
+        {
+            cal += int.Parse(line);
+        }
+        line = reader.ReadLine();
+    }
+    reader.Close();
+
+    Console.WriteLine(currMax);
+    Console.WriteLine(list.Max());
+    list.Sort();
+    list.Reverse();
+    Console.WriteLine(list[0] + list[1] + list[2]);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
